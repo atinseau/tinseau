d1cf013e4b1509d2c08b30a45d3b23ec63f67079
atinseau <arthurtweak@gmail.com>
2022-01-28 18:31:47 +0100

    default

--- wp-content/themes/tinseau/Includes/dynamic_attribute.php
+++ wp-content/themes/tinseau/Includes/dynamic_attribute.php
@@ -1,36 +1,36 @@
 <?php
 
-$event_date_attribute = "pa_event_date";
 
-add_action('init', function () use ($event_date_attribute) {
+
+add_action('admin_init', function () {
+
+    $event_date_attribute = "pa_event_date";
+
     // if the attribute doesn't exist, create it
-    if (!is_admin()) {
-        if (!taxonomy_exists($event_date_attribute)) {
-            $args = array(
-                'slug'    => 'event_date',
-                'name'   => 'Date de l\'évenement',
-                'type'    => 'select',
-                'orderby' => 'menu_order',
-                'id' => $event_date_attribute,
-                'has_archives'  => false,
-            );
-            wc_create_attribute( $args );
+    if (!taxonomy_exists($event_date_attribute)) {
+
+        $args = array(
+            'slug'    => 'event_date',
+            'name'   => 'Date de l\'évenement',
+            'type'    => 'select',
+            'orderby' => 'menu_order',
+            'id' => $event_date_attribute,
+            'has_archives'  => false,
+        );
+        $id = wc_create_attribute( $args );
+
+        $new_terms = array();
+        $query = new WP_Query(array(
+            'post_type' => 'product',
+            'posts_per_page'   => -1,
+            'product_cat' => 'tinseau-test-day',
+        ));
+
+        foreach ($query->posts as $post) {
+            $new_terms[] = $post->post_title;
         }
+
+        $res = wp_set_object_terms($id, $new_terms, $event_date_attribute);
+
     }
 });
-
-
-add_filter('woocommerce_attribute_added', function ($id) use ($event_date_attribute) {
-
-    $new_terms = array();
-    $query = new WP_Query(array(
-        'post_type' => 'product',
-        'posts_per_page'   => -1,
-        'product_cat' => 'tinseau-test-day',
-    ));
-    foreach ($query->posts as $post) {
-        $new_terms[] = $post->post_title;
-    }
-    $res = wp_set_object_terms($id, $new_terms, $event_date_attribute);
-}, 10, 3);
-
